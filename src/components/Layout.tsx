511f7759ed03813883fbc9d0a5bb373c8180f000
Su777 <yf.yan@icloud.com>
2026-07-20 13:29:02 +0800

    avatar & icon position update

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -39,7 +39,7 @@ export default function Layout() {
               </div>
             ) : (
               <img
-                src="/uploads/avatar.png"
+                src="/res/avatar.png"
                 alt="Su777"
                 onError={() => setAvatarError(true)}
                 className="w-10 h-10 rounded-full object-cover border border-line group-hover:border-clay transition-colors"
